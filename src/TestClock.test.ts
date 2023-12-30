115dfd5613a6c0ec5a59c85799e9668118b9a6e5
Chet Corcos <ccorcos@gmail.com>
2023-12-29 19:59:00 -0800

    More tests

--- src/TestClock.test.ts
+++ src/TestClock.test.ts
@@ -48,4 +48,116 @@ describe("TestClock", () => {
 
 		assert.deepEqual(log, [0, 1, 4, 5, 6, 10, 14, 15, 16, 24, 25, 26])
 	})
+
+	it("subsequent sleeps", async () => {
+		const { sleep, run } = new TestClock()
+		const log: number[] = []
+
+		const promises = [
+			(async () => {
+				await sleep(1)
+				log.push(1)
+				await sleep(1)
+				log.push(2)
+				await sleep(1)
+				log.push(3)
+				await sleep(1)
+				log.push(4)
+			})(),
+			(async () => {
+				await sleep(1.3)
+				log.push(1.3)
+				await sleep(1)
+				log.push(2.3)
+				await sleep(1)
+				log.push(3.3)
+				await sleep(1)
+				log.push(4.3)
+			})(),
+			(async () => {
+				await sleep(1.1)
+				log.push(1.1)
+				await sleep(1)
+				log.push(2.1)
+				await sleep(1)
+				log.push(3.1)
+				await sleep(1)
+				log.push(4.1)
+			})(),
+			(async () => {
+				await sleep(1.2)
+				log.push(1.2)
+				await sleep(1)
+				log.push(2.2)
+				await sleep(1)
+				log.push(3.2)
+				await sleep(1)
+				log.push(4.2)
+			})(),
+		]
+
+		await run()
+		await Promise.all(promises)
+
+		assert.deepEqual(
+			log,
+			[1, 1.1, 1.2, 1.3, 2, 2.1, 2.2, 2.3, 3, 3.1, 3.2, 3.3, 4, 4.1, 4.2, 4.3]
+		)
+	})
+
+	it("subsequent at the same time", async () => {
+		const { sleep, run } = new TestClock()
+		const log: number[] = []
+
+		const promises = [
+			(async () => {
+				await sleep(1)
+				log.push(1)
+				await sleep(1)
+				log.push(2)
+				await sleep(1)
+				log.push(3)
+				await sleep(1)
+				log.push(4)
+			})(),
+			(async () => {
+				await sleep(1)
+				log.push(1.3)
+				await sleep(1.3)
+				log.push(2.3)
+				await sleep(1)
+				log.push(3.3)
+				await sleep(1)
+				log.push(4.3)
+			})(),
+			(async () => {
+				await sleep(1)
+				log.push(1.1)
+				await sleep(1.1)
+				log.push(2.1)
+				await sleep(1)
+				log.push(3.1)
+				await sleep(1)
+				log.push(4.1)
+			})(),
+			(async () => {
+				await sleep(1)
+				log.push(1.2)
+				await sleep(1.2)
+				log.push(2.2)
+				await sleep(1)
+				log.push(3.2)
+				await sleep(1)
+				log.push(4.2)
+			})(),
+		]
+
+		await run()
+		await Promise.all(promises)
+
+		assert.deepEqual(
+			log,
+			[1, 1.1, 1.2, 1.3, 2, 2.1, 2.2, 2.3, 3, 3.1, 3.2, 3.3, 4, 4.1, 4.2, 4.3]
+		)
+	})
 })
